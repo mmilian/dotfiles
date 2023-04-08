37e2adcebd92a36a287116c0538e66f9cd967be4
mateusz.milian <mateusz.milian@gmail.com>
2023-04-08 10:04:19 +0200

    Add neovim

--- install/install-deps-linux.sh
+++ install/install-deps-linux.sh
@@ -3,5 +3,8 @@ apt-get install tree
 # apt-get install youtube-dl
 apt-get install json_pp
 # apt-get install nvm
+apt-get install fzf
+apt-get install ripgrep
+apt-get install fd-find
 apt-get install exa
 apt install zsh && chsh -s $(which zsh)
